21902ef3d474b984f9c1e96ff4e2411edecb2c0a
hzwangqiqing <hzwangqiqing@corp.netease.com>
2018-07-27 18:48:03 +0800

    #银行转账系统修改工程结构

--- bank-service/src/main/java/com/netease/kaola/bank/service/impl/AccountAmountServiceImpl.java
+++ bank-service/src/main/java/com/netease/kaola/bank/service/impl/AccountAmountServiceImpl.java
@@ -1,10 +1,33 @@
 package com.netease.kaola.bank.service.impl;
 
 import com.netease.kaola.bank.service.api.AccountAmountService;
+import com.netease.kaola.generic.api.bank.mapper.AccountAmountMapper;
+import com.netease.kaola.generic.api.model.TransactionRecords;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Created by hzwangqiqing
  * on 2018/7/27.
  */
+@Service("accountAmountService")
 public class AccountAmountServiceImpl implements AccountAmountService {
+    @Resource
+    private AccountAmountMapper accountAmountMapper;
+
+    @Override
+    public Integer updateAccountAmountRecordsPrepare(TransactionRecords transactionRecords) {
+        return null;
+    }
+
+    @Override
+    public Integer updateAccountAmountRecordsPrepareToConfirm(TransactionRecords transactionRecords) {
+        return null;
+    }
+
+    @Override
+    public Integer updateAccountAmountRecordsPrepareToCancel(TransactionRecords transactionRecords) {
+        return null;
+    }
 }
